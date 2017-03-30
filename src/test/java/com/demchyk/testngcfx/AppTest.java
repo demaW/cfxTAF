b7e0e75b188a2d9872ccbb113a5d162e31c91ce9
Volodymyr Demchyk <demavov@gmail.com>
2017-03-31 01:42:33 +0300

    added some service call

--- src/test/java/com/demchyk/testngcfx/AppTest.java
+++ src/test/java/com/demchyk/testngcfx/AppTest.java
@@ -1,11 +1,22 @@
 package com.demchyk.testngcfx;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import org.apache.cxf.jaxrs.client.WebClient;
 import org.testng.annotations.Test;
 
+import javax.ws.rs.core.MediaType;
+import java.util.Collections;
+
+
 public class AppTest{
+    public static final String URI ="http://www.omdbapi.com/";
+    public static final String ID = "i=tt1520211";
 
     @Test
     public void testOmdb(){
+        WebClient client = WebClient.create(URI, Collections.singletonList(new JacksonJsonProvider())).query(ID).type(MediaType.APPLICATION_JSON);
+        OMDBEntry omdb = client.get(OMDBEntry.class);
+        System.out.println(omdb.toString());
 
     }
 }
